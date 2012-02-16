7fdf65c32c59c2c875dd7d3307f80b8485ed735a
ziqew <gongwenwei@gmail.com>
2012-02-16 13:33:57 +0800

    add cs106b stanfordCPPLib

---
+++ cs106b/Assignment1-vs2008/StanfordCPPLib/include/private/main.h
@@ -0,0 +1,75 @@
+/*
+ * File: main.h
+ * ------------
+ * This file renames the <code>main</code> method in the client's
+ * program to <code>Main</code>, thereby allowing a custom
+ * <code>main</code> method in the libraries to take control
+ * before passing control back to the client program.  The main macro
+ * also defines a function getMainFlags that returns an int whose bits
+ * indicate which of the various interfaces have been loaded by this
+ * definition of main.
+ *
+ * Note: This file can be loaded more than once and must therefore
+ * check to see what has already been defined.
+ */
+
+#ifdef main
+#  undef main
+#  undef CONSOLE_FLAG
+#  undef GRAPHICS_FLAG
+#else
+#  define MAIN_USES_CONSOLE (1<<0)
+#  define MAIN_USES_GRAPHICS (1<<1)
+#endif
+
+#ifdef _console_h
+#  define CONSOLE_FLAG MAIN_USES_CONSOLE
+#else
+#  define CONSOLE_FLAG 0
+#endif
+
+#ifdef _graphics_h
+#  define GRAPHICS_FLAG MAIN_USES_GRAPHICS
+#else
+#  define GRAPHICS_FLAG 0
+#endif
+
+#if defined(_MSC_VER)
+
+#define main main(int argc, char **argv) { \
+	            extern int Main(); \
+	            extern int (*_mainCallback)(); \
+                extern int _mainFlags; \
+                _mainFlags = 0; \
+				_mainCallback = Main; \
+                return mainWrapper(); } \
+             int Main
+
+extern int mainWrapper();
+
+#else
+
+#if CONSOLE_FLAG | GRAPHICS_FLAG
+
+#define main main(int argc, char **argv) { \
+                extern int _mainFlags; \
+                _mainFlags = GRAPHICS_FLAG + CONSOLE_FLAG; \
+                return startupMain(argc, argv); \
+             } \
+             int Main
+
+extern int startupMain(int argc, char **argv);
+
+#else
+
+#define main main(int argc, char **argv) { \
+                extern int _mainFlags; \
+                _mainFlags = 0; \
+                return mainWrapper(); } \
+             int Main
+
+extern int mainWrapper();
+
+#endif
+#endif
+
