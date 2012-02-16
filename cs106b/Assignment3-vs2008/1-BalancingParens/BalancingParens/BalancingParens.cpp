1e69c7a27924c91d6f9b53ff79df49b023c877cc
ziqew <gongwenwei@gmail.com>
2012-02-16 13:34:36 +0800

    add cs106b assignment3

---
+++ cs106b/Assignment3-vs2008/1-BalancingParens/BalancingParens/BalancingParens.cpp
@@ -0,0 +1,39 @@
+/*
+ * File: BalancingParens.cpp
+ * -------------------------
+ * Name: [TODO: enter name here]
+ * Section: [TODO: enter section leader here]
+ * This file is the starter project for the balancing parentheses problem on
+ * Assignment #3
+ * [TODO: extend the documentation]
+ */
+
+#include <iostream>
+#include "console.h"
+#include "simpio.h"
+using namespace std;
+
+/* Function prototypes */
+
+bool isBalanced(string exp);
+
+/* Main program */
+
+int main() {
+   // [write a test program here]
+   return 0;
+}
+
+/*
+ * Function: isBalanced
+ * Usage: if (isBalanced(exp)) . . .
+ * ---------------------------------
+ * Returns true if the string exp is balanced, which means that it
+ * is either (a) empty or (b) contains a consecutive paired operator
+ * and the string is balanced if you remove that operator.
+ */
+
+bool isBalanced(string exp) {
+   // [Fill in this code]
+   return false;
+}
---
+++ cs106b/Assignment3-vs2008/3-SierpinskiTriangle/SierpinskiTriangle/SierpinskiTriangle.cpp
@@ -0,0 +1,21 @@
+/*
+ * File: SierpinskiTriangle.cpp
+ * ----------------------------
+ * Name: [TODO: enter name here]
+ * Section: [TODO: enter section leader here]
+ * This file is the starter code for the Sierpinski Triangle problem
+ * fron Assignment #3.
+ * [TODO: extend the documentation]
+ */
+
+#include <iostream>
+#include "graphics.h"
+using namespace std;
+
+/* Main program */
+
+int main() {
+   initGraphics();
+   // Fill in the necessary code here
+   return 0;
+}
