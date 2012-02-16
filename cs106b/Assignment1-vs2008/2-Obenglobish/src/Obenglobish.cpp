11183016eb1e2c4576849594b84431d41619624f
ziqew <gongwenwei@gmail.com>
2012-02-16 13:32:27 +0800

    add cs106b obenglobish

---
+++ cs106b/Assignment1-vs2008/2-Obenglobish/src/Obenglobish.cpp
@@ -0,0 +1,21 @@
+/*
+ * File: Obenglobish.java
+ * ----------------------
+ * Name: [TODO: enter name here]
+ * Section: [TODO: enter section leader here]
+ * This file is the starter project for the Obenglobish problem.
+ * [TODO: rewrite the documentation]
+ */
+
+#include <iostream>
+#include <string>
+#include "simpio.h"
+#include "strlib.h"
+using namespace std;
+
+/* Main program */
+
+int main() {
+   // [TODO: fill in the code]
+   return 0;
+}
