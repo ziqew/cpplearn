2b28852639a1730ca99581bd09d7994525f9aebd
ziqew <gongwenwei@gmail.com>
2012-02-16 13:33:33 +0800

    add cs106b numericConversion

---
+++ cs106b/Assignment1-vs2008/4-NumericConversion/src/NumericConversion.cpp
@@ -0,0 +1,25 @@
+/*
+ * File: NumericConversion.cpp
+ * ---------------------------
+ * Name: [TODO: enter name here]
+ * Section: [TODO: enter section leader here]
+ * This file is the starter project for the numeric-conversion problem
+ * in which you implement the functions intToString and stringToInt.
+ * [TODO: rewrite the documentation]
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+/* Function prototypes */
+
+string intToString(int n);
+int stringToInt(string str);
+
+/* Main program */
+
+int main() {
+   // [TODO: fill in the code]
+   return 0;
+}
