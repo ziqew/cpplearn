873ae26bc8db0c4b87c8839c00284a0db03677b3
ziqew <gongwenwei@gmail.com>
2012-02-16 13:33:02 +0800

    add cs106b combinations

---
+++ cs106b/Assignment1-vs2008/3-Combinations/src/Combinations.cpp
@@ -0,0 +1,17 @@
+/*
+ * File: Combinations.cpp
+ * ----------------------
+ * Name: [TODO: enter name here]
+ * Section: [TODO: enter section leader here]
+ * This file is the starter project for the recursive combinations problem.
+ * [TODO: rewrite the documentation]
+ */
+
+#include <iostream>
+#include "simpio.h"
+using namespace std;
+
+int main() {
+   // [TODO: fill in the code]
+   return 0;
+}
